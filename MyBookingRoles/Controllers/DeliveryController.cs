2965bec00add900e7b664f9d40a64bc1168d3996
21533697 <51156505+21533697@users.noreply.github.com>
2020-11-26 05:59:00 +0200

    Update DeliveryController.cs

--- MyBookingRoles/Controllers/DeliveryController.cs
+++ MyBookingRoles/Controllers/DeliveryController.cs
@@ -1,4 +1,5 @@
-﻿using MyBookingRoles.Models;
+﻿using Microsoft.AspNet.Identity;
+using MyBookingRoles.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,21 +12,40 @@ namespace MyBookingRoles.Controllers
     public class DeliveryController : Controller
     {
         private ApplicationDbContext context = new ApplicationDbContext();
-
-        // GET: Delivery All Delivery Orders Approved
-        public ActionResult DeliveryIndex()
-        {
-            return View(context.Orders.Where(o => o.Status == "Approved").ToList());
-        }
-
+        
+        //
         public ActionResult DeliveryDashboard()
         {
             return View();
         }
-
+        
+        // GET: Delivery All Delivery Jobs Approved
         public ActionResult ApprovedOrders(string searchWord)
         {
-            return View(context.Orders.Where(p => p.Status == "Approved" && p.CustomerAddress.Contains(searchWord) || searchWord == null).ToList());
+            return View(context.Orders.Where(p => p.Status == "Approved" || p.CustomerAddress.Contains(searchWord) || searchWord == null).ToList());
+        }
+
+        // Get : AcceptOrder
+        [HttpPost]
+        public ActionResult AcceptOrder(int id)
+        {
+            var deliverId = User.Identity.GetUserName().ToString();
+
+            if(deliverId != null)
+            {
+                //Create Job Class
+                //Attr: JobId,OrderId,DeliveryPerson,
+                //Attr: InvoicePdf,InvoiceDownloaded,JobStatus,
+                //Attr: AcceptedDate,DeliveryDate
+
+                //Save Changes To Database
+
+                return RedirectToAction("ApprovedOrders");
+            }
+            else
+            {
+                return RedirectToAction("Details","Orders");
+            }
         }
     }
 }
